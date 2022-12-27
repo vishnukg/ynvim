641699d4c23df0cbc13b7a89b6bb37c955471bdb
Vishnu Ganesan <vishnukg@gmail.com>
2022-12-27 22:55:59 +1100

    Added more commands

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,1 +1,11 @@
 vim.keymap.set("n", "<C-g>", vim.cmd.Ex)
+
+
+vim.keymap.set("n","<leader><leader>",":wa<CR>")
+
+
+
+vim.keymap.set("n", "<C-n>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-p>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>n", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>p", "<cmd>lprev<CR>zz")
