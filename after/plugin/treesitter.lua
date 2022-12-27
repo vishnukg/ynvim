8cef8666d04eb82d975fd5c6eaba39b3cfe97e2e
Vishnu Ganesan <vishnukg@gmail.com>
2022-12-27 21:57:38 +1100

    Added elixir treesitter and harpoon

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,7 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "help", "javascript", "typescript", "c", "lua", "rust", "vim", "go", "scala", "python"},
+  ensure_installed = { "help", "javascript", "typescript", "c", "lua", "rust", "vim", "go", "scala", "python", 
+  "elixir"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
